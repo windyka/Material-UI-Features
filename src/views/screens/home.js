23b4ef95ab46b818391031277413d459c807ac0a
windyka <officialwindyka@gmail.com>
2019-01-22 22:02:27 +0700

    Seting Up Material ui + create header app

---
+++ src/views/screens/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AppHeader from '../component/appBar';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <AppHeader />
+      </div>
+    );
+  }
+}
+
+export default Home;
