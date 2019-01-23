4855f518224f37f35eea6b4e63c379d9a554b678
windyka <officialwindyka@gmail.com>
2019-01-24 01:20:44 +0700

    AppBar with Menu

--- src/views/screens/home.js
+++ src/views/screens/home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 // import AppHeader from '../component/appBar';
-import PrimarySearchAppBar from '../component/appBarPrimary';
+// import PrimarySearchAppBar from '../component/appBarPrimary';
+import MenuAppBar from '../component/appBarMenu';
 
 class Home extends Component {
   render() {
     return (
       <div>
         {/* <AppHeader /> */}
-        <PrimarySearchAppBar />
+        {/* <PrimarySearchAppBar /> */}
+        <MenuAppBar />
       </div>
     );
   }
